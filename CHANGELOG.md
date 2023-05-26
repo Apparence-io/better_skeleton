0c1db705783b45d2de106fd2b5491f9013d73120
Gautier <gautier@apparence.io>
2023-05-26 19:27:57 +0200

    dart 3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
+## 0.0.2
+- dart 3 migration
 ## 0.0.1
-
-* TODO: Describe initial release.
+- skeleton builder
--- test/better_skeleton_test.dart
+++ test/better_skeleton_test.dart
@@ -1,10 +1,6 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:better_skeleton/better_skeleton.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('better_skeleton');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
   group('one skeleton widget', () {
